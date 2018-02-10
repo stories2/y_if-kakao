fbdf7aafdf8aee049ed569179d27c8dce797faa2
Kim HyunWoo <stories2@naver.com>
2018-02-11 00:09:06 +0900

    ProcessManager module : remove special character in order

--- Core/ProcessManager.py
+++ Core/ProcessManager.py
@@ -1,13 +1,16 @@
-from Utils import LogManager
+from Utils import LogManager, ResponseManager
 from Setting import DefineManager
 from Core import LanguageManager, SystemManager
+import re
 
 def ExecuteOrder(orderDic):
     orderArgs = []
     orderArgs = orderDic["text"].split("  ")
-    orderArgs[DefineManager.ORDER_SAVED_POINT] = orderArgs[DefineManager.ORDER_SAVED_POINT].replace("!", "")
+    orderArgs[DefineManager.ORDER_SAVED_POINT] = re.sub("[?|<|>|?|:|/|!]", "", orderArgs[DefineManager.ORDER_SAVED_POINT]) #orderArgs[DefineManager.ORDER_SAVED_POINT].replace("!", "")
     LogManager.PrintLogMessage("ProcessManager", "ExecuteOrder", "execute order args: " + " | ".join(orderArgs), DefineManager.LOG_LEVEL_INFO)
     if orderArgs[DefineManager.ORDER_SAVED_POINT] == "translate":
         return LanguageManager.OriginLanguageToTargetLanguage(orderArgs[2], orderArgs[1])
     elif orderArgs[DefineManager.ORDER_SAVED_POINT] == "echo":
         return SystemManager.Echo(orderArgs[1])
+    else:
+        return ResponseManager.TemplateOfResponse(DefineManager.SIMPLE_RESPONSE, orderArgs[DefineManager.ORDER_SAVED_POINT])
